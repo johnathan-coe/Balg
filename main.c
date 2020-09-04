bc4478e3a88a4587b13a8aeb4c6660d357f5522a
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2020-09-04 14:26:32 +1200

    Refined truth table generator, argument order now specified in second argument

--- main.c
+++ main.c
@@ -35,20 +35,25 @@ SExpression *getAST(const char *expr)
     return expression;
 }
 
-int evaluate(SExpression *e, int input)
+int evaluate(SExpression *e, int input, int symbols[])
 {
     switch (e->type) {
         case eVALUE:
-	    return ((1 << (e->value - 97)) & input) != 0;
+	    if (symbols[e->value - 97] == -1) {
+		    printf("FATAL: %c not mapped! Specify a position for %c in the second argument\n", e->value, e->value);
+		    exit(0);
+	    }
+
+	    return ((1 << symbols[e->value - 97]) & input) != 0;
         case eOR:
-            return evaluate(e->left, input) || evaluate(e->right, input);
+            return evaluate(e->left, input, symbols) || evaluate(e->right, input, symbols);
         case eAND:
-            return evaluate(e->left, input) && evaluate(e->right, input);
+            return evaluate(e->left, input, symbols) && evaluate(e->right, input, symbols);
 	case eNOT:
-            return !evaluate(e->left, input);
+            return !evaluate(e->left, input, symbols);
 	case eIMPLY:
-	    if (evaluate(e->left, input)) {
-	        return evaluate(e->right, input);
+	    if (evaluate(e->left, input, symbols)) {
+	        return evaluate(e->right, input, symbols);
 	    } else {
 		return 1;
 	    }
@@ -61,14 +66,39 @@ int evaluate(SExpression *e, int input)
 int main(int argc, char *args[])
 {
     SExpression *e = getAST(args[1]);
+	
+    // Mapping of symbols to position in input integer
+    int symbols[26];
+    for (int i = 0; i < 26; i++) { symbols[i] = -1; }
+ 
+    int numSymbols = strlen(args[2]);
 
-    printf("c b a =\n");
+    // Populate symbol table, this maps each letter to a bit in input
+    for (int i = 0; i <= numSymbols; i++) {
+	    symbols[args[2][i] - 97] = numSymbols - 1 - i;
+    }
+ 
+    int rows = (1 << (numSymbols));
 
-    for (int i = 0; i < 8; i++) {
-	    int result = evaluate(e, i);
-	    printf("%d %d %d %d\n", (4 & i) != 0, (2 & i) != 0, (1 & i) != 0, result);
+    // Print header
+    for (int i = 0; i < numSymbols; i++) {
+	printf("%c ", args[2][i]);
     }
 
+    printf("\n");
+
+    // Generate truth table
+    for (int i = 0; i < rows; i++) {
+	    int result = evaluate(e, i, symbols);
+	
+	    for (int s = numSymbols - 1; s >= 0; s--) {
+		printf("%d ", ((1 << s) & i) != 0);
+	    }
+
+	    printf("%d\n", result);
+    }
+	
+    // Free memory
     deleteExpression(e);
     return 0;
 }
