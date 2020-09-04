3e600366953652c4862061f8e488c5191f7468b7
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2020-09-04 15:08:39 +1200

    Move AST creation and evaluation out of main.c

---
+++ AST/Util.h
@@ -0,0 +1,4 @@
+#include "Expression.h"
+
+int evaluate(SExpression *e, int input, int symbols[]);
+SExpression *getAST(const char *expr);
--- main.c
+++ main.c
@@ -2,66 +2,11 @@
  * main.c file
  */
 
+#include "AST/Util.h"
 #include "AST/Expression.h"
-#include "Parser/Parser.h"
-#include "Lexer/Lexer.h"
 
 #include <stdio.h>
-
-int yyparse(SExpression **expression, yyscan_t scanner);
-
-SExpression *getAST(const char *expr)
-{
-    SExpression *expression;
-    yyscan_t scanner;
-    YY_BUFFER_STATE state;
-
-    if (yylex_init(&scanner)) {
-        /* could not initialize */
-        return NULL;
-    }
-
-    state = yy_scan_string(expr, scanner);
-
-    if (yyparse(&expression, scanner)) {
-        /* error parsing */
-        return NULL;
-    }
-
-    yy_delete_buffer(state, scanner);
-
-    yylex_destroy(scanner);
-
-    return expression;
-}
-
-int evaluate(SExpression *e, int input, int symbols[])
-{
-    switch (e->type) {
-        case eVALUE:
-	    if (symbols[e->value - 97] == -1) {
-		    printf("FATAL: %c not mapped! Specify a position for %c in the second argument\n", e->value, e->value);
-		    exit(0);
-	    }
-
-	    return ((1 << symbols[e->value - 97]) & input) != 0;
-        case eOR:
-            return evaluate(e->left, input, symbols) || evaluate(e->right, input, symbols);
-        case eAND:
-            return evaluate(e->left, input, symbols) && evaluate(e->right, input, symbols);
-	case eNOT:
-            return !evaluate(e->left, input, symbols);
-	case eIMPLY:
-	    if (evaluate(e->left, input, symbols)) {
-	        return evaluate(e->right, input, symbols);
-	    } else {
-		return 1;
-	    }
-        default:
-            /* should not be here */
-            return 0;
-    }
-}
+#include <string.h>
 
 int main(int argc, char *args[])
 {
